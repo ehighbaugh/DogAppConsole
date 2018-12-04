997591b77e6355a2785eebadd44575d97f325cf7
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-12-03 20:22:35 -0500

    Adding tables and creating DataSets and DataTableCollections

--- DogAppConsole/Program.cs
+++ DogAppConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,29 +10,56 @@ namespace DogAppConsole
 {
     class Program
     {
-        /// <summary>
-        /// This method attempts to create the Dogs1 SQL table.
-        /// If will do nothing but print an error if the table already exists.
-        /// </summary>
-        static void TryCreateTable()
+        static void Main()
         {
-            using (SqlConnection con = new SqlConnection(
-                Program.Properties.Settings.Default.masterConnectionString))
+            DataTable table1 = new DataTable("dogs");
+            table1.Columns.Add("name");
+            table1.Columns.Add("age");
+            table1.Rows.Add("spot", 1);
+            table1.Rows.Add("buddy", 2);
+
+            DataTable table2 = new DataTable("breeds");
+            table2.Columns.Add("breed");
+            table2.Columns.Add("color");
+            table2.Rows.Add("boston terrier", "black/white");
+            table2.Rows.Add("boxer", "brindle");
+
+            // create DataSet and put both tables in it
+            //using (DataSet set = new DataSet("dogBreeds"))
+            //{
+            //    set.Tables.Add(table1);
+            //    set.Tables.Add(table2);
+            //    set.Namespace = "y";
+            //    set.Prefix = "x";
+
+            //    // copy dataset
+            //    DataSet copy = set.Copy();
+
+            //    // visualize dataset
+            //    Console.WriteLine("set: {0}", set.GetXml());
+            //    Console.WriteLine("set: {1}", copy.GetXml());
+            //}
+
+            DataSet set = new DataSet("dogBreeds2");
+            set.Tables.Add(table1);
+            set.Tables.Add(table2);
+
+            // loop over datatables in dataset
+            DataTableCollection collection = set.Tables;
+            for (int i = 0; i < collection.Count; i++)
             {
-                con.Open();
-                try
-                {
-                    using (SqlCommand command = new SqlCommand(
-                        "CREATE TABLE Dogs1 (Weight INT, Name TEXT, Breed TEXT)", con))
-                    {
-                        command.ExecuteNonQuery();
-                    }
-                }
-                catch
-                {
-                    Console.WriteLine("Table not created.");
-                }
+                DataTable table = collection[i];
+                Console.WriteLine("{0}: {1}", i, table.TableName);
             }
+
+            // write first table name
+            Console.WriteLine("x: {0}", set.Tables[0].TableName);
+
+            // write row count of breeds table
+            Console.WriteLine("y: {0}", set.Tables["breeds"].Rows.Count);
+            Console.ReadLine();
+
+            
         }
     }
 }
