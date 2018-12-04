012696d2b93d9a193d656f5f24bc56e6bd61fce8
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-12-03 19:50:28 -0500

    Initializing program

--- DogAppConsole/Program.cs
+++ DogAppConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,29 @@ namespace DogAppConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// This method attempts to create the Dogs1 SQL table.
+        /// If will do nothing but print an error if the table already exists.
+        /// </summary>
+        static void TryCreateTable()
         {
+            using (SqlConnection con = new SqlConnection(
+                Program.Properties.Settings.Default.masterConnectionString))
+            {
+                con.Open();
+                try
+                {
+                    using (SqlCommand command = new SqlCommand(
+                        "CREATE TABLE Dogs1 (Weight INT, Name TEXT, Breed TEXT)", con))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Table not created.");
+                }
+            }
         }
     }
 }
